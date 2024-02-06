7515865e41bb4ec4e2e1986e7ac40cee5f6b1089
DeJBoi <306012@eriesd.org>
2024-02-06 14:35:27 -0500

    :

--- Alphabet Soup/problem.py
+++ Alphabet Soup/problem.py
@@ -6,16 +6,7 @@ Input: ""hooplah""
 Output: ahhloop
 """
 def alphabet_soup(word:str) ->str:
-        """
-    This function alphabet_soup(str) take the str string parameter being passed 
-    and return the string with the letters in alphabetical order 
-    (ie. hello becomes ehllo). Convert uppercase letters to lowercase.
-    Input: ""coderbyte""
-    Output: bcdeeorty
-    Input: ""hooplah""
-    Output: ahhloop
-    """ 
-    pass
+    
 
 
 print(alphabet_soup("Good Luck!"))
