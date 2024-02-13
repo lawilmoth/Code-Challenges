3d7426355d7066ec4f3ca92d878098f8aa04a141
DeJBoi <306012@eriesd.org>
2024-02-13 15:34:21 -0500

    AB Check, more test

--- AB Check/tests.py
+++ AB Check/tests.py
@@ -8,6 +8,21 @@ class TestSolution(unittest.TestCase):
                         f'Expected "False", but got {ab_check("after badly")}'
                         )
         
+        self.assertEqual(ab_check('Lil Trey'), 
+                        False,
+                        f'Expected "False", but got {ab_check("after badly")}'
+                        )
+        
+        self.assertEqual(ab_check(''), 
+                        False,
+                        f'Expected "False", but got {ab_check("after badly")}'
+                        )
+        
+        self.assertEqual(ab_check('arlen mcbeth'), 
+                        False,
+                        f'Expected "False", but got {ab_check("after badly")}'
+                        )
+        
     def test_a_b_true(self):
         self.assertEqual(ab_check('Laura sobs'), 
                         True,
@@ -17,6 +32,27 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(ab_check('Aura'),
                         True,
                         f'Expected "True", but got {ab_check("Aura")}')
+        
+        self.assertEqual(ab_check('Bilb'),
+                        True,
+                        f'Expected "True", but got {ab_check("Bilb")}')
+        
+        self.assertEqual(ab_check('AAAAAAAAAAAB'),
+                        True,
+                        f'Expected "True", but got {ab_check("Bilb")}')
+        
+        self.assertEqual(ab_check('ABBBBBB'),
+                        True,
+                        f'Expected "True", but got {ab_check("Bilb")}')
+        
+        self.assertEqual(ab_check('All Balla'),
+                        True,
+                        f'Expected "True", but got {ab_check("Bilb")}')
+
+        self.assertEqual(ab_check('lane borrowed'),
+                        True,
+                        f'Expected "True", but got {ab_check("Bilb")}')
+        
 
 if __name__ == "__main__":
     unittest.main()
