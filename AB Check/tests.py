14bda724a0a03fe165b6d7e3406440a5b741f446
DeJBoi <306012@eriesd.org>
2024-02-13 15:34:45 -0500

    More Tests

--- AB Check/tests.py
+++ AB Check/tests.py
@@ -2,6 +2,8 @@ import unittest
 from problem import ab_check
 
 class TestSolution(unittest.TestCase):
+
+#false
     def test_a_b_false(self):
         self.assertEqual(ab_check('after badly'), 
                         False,
@@ -10,19 +12,31 @@ class TestSolution(unittest.TestCase):
         
         self.assertEqual(ab_check('Lil Trey'), 
                         False,
-                        f'Expected "False", but got {ab_check("after badly")}'
+                        f'Expected "False", but got {ab_check("Lil Trey")}'
                         )
         
         self.assertEqual(ab_check(''), 
                         False,
-                        f'Expected "False", but got {ab_check("after badly")}'
+                        f'Expected "False", but got {ab_check("")}'
                         )
         
         self.assertEqual(ab_check('arlen mcbeth'), 
                         False,
-                        f'Expected "False", but got {ab_check("after badly")}'
+                        f'Expected "False", but got {ab_check("arlen mcbeth")}'
+                        )
+
+        self.assertEqual(ab_check(1234), 
+                        False,
+                        f'Expected "False", but got {ab_check(1234)}'
                         )
         
+
+        self.assertEqual(ab_check('1234'), 
+                        False,
+                        f'Expected "False", but got {ab_check("1234")}'
+                        )
+        
+#true        
     def test_a_b_true(self):
         self.assertEqual(ab_check('Laura sobs'), 
                         True,
@@ -39,19 +53,19 @@ class TestSolution(unittest.TestCase):
         
         self.assertEqual(ab_check('AAAAAAAAAAAB'),
                         True,
-                        f'Expected "True", but got {ab_check("Bilb")}')
+                        f'Expected "True", but got {ab_check("AAAAAAAAAAAB")}')
         
         self.assertEqual(ab_check('ABBBBBB'),
                         True,
-                        f'Expected "True", but got {ab_check("Bilb")}')
+                        f'Expected "True", but got {ab_check("ABBBBBB")}')
         
         self.assertEqual(ab_check('All Balla'),
                         True,
-                        f'Expected "True", but got {ab_check("Bilb")}')
+                        f'Expected "True", but got {ab_check("All Balla")}')
 
         self.assertEqual(ab_check('lane borrowed'),
                         True,
-                        f'Expected "True", but got {ab_check("Bilb")}')
+                        f'Expected "True", but got {ab_check("lane borrowed")}')
         
 
 if __name__ == "__main__":
